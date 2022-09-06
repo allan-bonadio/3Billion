9cedae3be587802ba205d12e481d4eea9e124fa3
Allan Bonadio <bojnac@tactileint.org>
2022-09-05 22:59:37 -0700

    I forgot the 22nd chromo! somewhere between 0...21 and 1...22 . fixed.

--- src/genome/genome.js
+++ src/genome/genome.js
@@ -2,16 +2,9 @@
 ** genome -- data structure that repsrensets a human's whole genome
 ** Copyright (C) 2022-2022 Tactile Interactive, all rights reserved
 */
-/* eslint-disable no-unused-vars */
 
 import dnaObject from '../dnaObject.js';
 import chromo from '../chromo/chromo.js';
-import arm from '../arm/arm.js';
-import gene from '../gene/gene.js';
-import codon from '../codon/codon.js';
-
-import formulate from '../formulate.js';
-import {codonToAmino} from '../dnaData.js';
 
 class genome extends dnaObject {
 	constructor(name, formula, gender) {
@@ -30,11 +23,11 @@ class genome extends dnaObject {
 		const list = this.list = new Array(23);
 
 		// first the numbered chromosomes
-		for (let i = 1; i < 22; i++)
+		for (let i = 1; i <= 22; i++)
 			list[i] = new chromo(i, formula.newFormula());
 
 		// then the xx or xy pair
-		list[22] = new chromo(this.gender, formula.newFormula());
+		list.push(new chromo(this.gender, formula.newFormula()));
 	}
 
 }
