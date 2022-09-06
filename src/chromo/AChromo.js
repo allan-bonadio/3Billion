e605a175d480c2eff4b7dab6f878750e3308258d
Allan Bonadio <bojnac@tactileint.org>
2022-09-05 22:59:37 -0700

    removed 'mother and father' paragraph

--- src/chromo/AChromo.js
+++ src/chromo/AChromo.js
@@ -2,28 +2,22 @@
 ** AChromo -- part of the 3 Billion and Me project
 ** Copyright (C) 2022-2022 Tactile Interactive, all rights reserved
 */
-/* eslint-disable no-unused-vars */
 
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import ArmInChromo from './ArmInChromo.js';
 import BackButton from '../BackButton.js';
 
-import genome from '../genome/genome.js';
 import chromo from './chromo.js';
-import arm from '../arm/arm.js';
-import gene from '../gene/gene.js';
-import codon from '../codon/codon.js';
 
-import {codonToAmino} from '../dnaData.js';
+//import {codonToAmino} from '../dnaData.js';
 
 import armsInChromoImage from './armsInChromo2.png';
 
 
 function AChromo(props) {
 	let {theChromo, style} = props;
-	let name = theChromo.name;
 
 	theChromo.populate();
 	//console.info(`theChromo.list: `, theChromo.list);
@@ -31,12 +25,9 @@ function AChromo(props) {
 		<ArmInChromo theArm={theArm} theChromo={theChromo} key={theArm.name} />);
 
 	return (<>
-		<div className='AChromo viewingPanel' key='chromo' style={style}>
+		<div id='chromo' className='AChromo viewingPanel' key='chromo' style={style}>
 			<BackButton title='Genome' level='chromo' />
 			<h3>Chromosome {theChromo.name}</h3>
-			The left and right chromatids come from the mother and father,
-			but figuring out which came from where is difficult.
-			So the labels 'left' and 'right' are mostly meaningless.
 			<div className='chromoImage'
 					onMouseMove={ev => ev.preventDefault()}>
 				<img src={armsInChromoImage}  alt='parts of a chromosome'/>
