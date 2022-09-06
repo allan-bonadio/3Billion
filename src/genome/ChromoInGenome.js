99efbde703fa0432eb0cea4488c25b86b44cbdcc
Allan Bonadio <bojnac@tactileint.org>
2022-09-05 22:59:37 -0700

    put click handler in separate func

--- src/genome/ChromoInGenome.js
+++ src/genome/ChromoInGenome.js
@@ -2,33 +2,35 @@
 ** ChromoInGenome -- part of the 3 Billion and Me project
 ** Copyright (C) 2022-2022 Tactile Interactive, all rights reserved
 */
-/* eslint-disable no-unused-vars, eqeqeq */
+/* eslint-disable eqeqeq */
 
-import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 import genome from '../genome/genome.js';
 import chromo from '../chromo/chromo.js';
-import arm from '../arm/arm.js';
-import gene from '../gene/gene.js';
-import codon from '../codon/codon.js';
 
 import ThreeBillion from '../ThreeBillion.js';
+
 
 function ChromoInGenome(props) {
 	const {theChromo} = props;
 	const name = theChromo.name;
 	let icon = <img className='chromoIcon'
 				src={`chromos/ch${name}.png`}
-				alt={'chromosome '+ name} />;
+				alt={`chromosome ${name}`} />;
+
+	// mouse up, but only if user didn't drag away
+	const handleClick =
+	ev =>  {
+		console.info(`ChromoInGenome: clicked and going lower...`);
+		ThreeBillion.navigateLower('chromo', theChromo);
+		ev.preventDefault();
+		ev.stopPropagation();
+	}
 
 	return (<>
-		<div className='ChromoInGenome clickable' key={name}
-				onClick={ev => {
-					ThreeBillion.navigateIn('chromo', theChromo);
-					ev.preventDefault();
-					ev.stopPropagation();
-				}}>
+		<div className={`ChromoInGenome clickable`} key={name}
+				onClick={handleClick} >
 			{icon}
 			<div>{name}</div>
 		</div>
